20fb06506517debbfab0b0ecb82ec5aed93f0ae0
Julian Weidner <julian.nicholas.weidner@gmail.com>
2018-09-22 23:04:11 -0500

    created a function that adds multiple Dom generated Div elements.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function GenerateDiv(){
   const containerDiv = document.getElementById('sketch-container');
   const pixelDiv = document.createElement('div');
-  pixelDiv.innerHTML = "Dom Div";
+  pixelDiv.innerHTML = "Dom Div "; //used for testing
   containerDiv.appendChild(pixelDiv);
 }
 
-GenerateDiv();
+
+
+function CreateSketchRow(btnVal){
+  let row = btnVal;
+  for(let i = 0; row > i; i++){
+      GenerateDiv();
+  }
+}
+
+function CreateSketchBoard(btnVal){
+  let row = btnVal;
+  for(let j = 0; j < row; j++){
+      let divParent = GenerateDiv(row);
+      const lineBr = document.createElement('br');
+      divParent.appendChild(lineBr);
+  }
+}
+
+CreateSketchBoard(4);
+
+
