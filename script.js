42074964d22d404496e570b5ca09c2a01db861f3
Julian Weidner <julian.nicholas.weidner@gmail.com>
2018-09-25 05:38:05 -0500

    Sizing and black color change 100% working. Cleaned up code and renamed vars too

--- script.js
+++ script.js
@@ -1,51 +1,63 @@
 
 //Event Handler
-sizeBtnGroup = document.querySelectorAll(".btn");
+sizeBtnGroup = document.querySelectorAll(".size-button");
 Array.from(sizeBtnGroup).map(i => AddEvent(i));
 
-//creates div for pixel divs
-function GenerateRow(){
-  const rowDiv = document.createElement('div');
-  rowDiv.classList.add('row-container'); //these are really columns
-  return rowDiv;
-}
 
-function GeneratePixel(rowParam){
+
+//Creates the Column Div and adds the style class to it
+//the div created by this function is held in memory, and not added to HTML until the GenerateTable()
+function GenerateColumn(){
+  const columnDiv = document.createElement('div');
+  columnDiv.classList.add('column-container'); 
+  return columnDiv;
+}
+//Creates a single Pixel
+//Adds the pixel to the Column Div parameneter (passed from Generate Table)
+function GeneratePixel(columnParam){
   const pixelDiv = document.createElement('div');
-  const rowDiv = rowParam;
-  pixelDiv.classList.add("pixel-container");
-  
-  rowDiv.appendChild(pixelDiv);
+  const columnDiv = columnParam;
+  pixelDiv.classList.add("pixel");
+  columnDiv.appendChild(pixelDiv);
 }
-
-function GenerateTable(columns){
-  const tableContainer = document.getElementById('sketch-container');
-  let row = GenerateRow();
-  tableContainer.appendChild(row);
-  let numOfPixels = columns;
+//Creates A Single Column
+//Adds column to the sketch container
+///generates x num of pixels and places them inside of the column 
+function GenerateTable(btnValParam){ 
+  const sketchContainer = document.getElementById('sketch-container');
+  let column = GenerateColumn(); //creates on column
+  sketchContainer.appendChild(column);
+  let numOfPixels = btnValParam; //The number of pixels is the same as the size button value
   for(let i = 0; i < numOfPixels; i++){
-    GeneratePixel(row);
+    GeneratePixel(column);
   }
 }
 
 function AddEvent(sizeBtnArrayParam){
   sizeBtnArrayParam.addEventListener('click', (e) => {
-    let columns = e.target.value; //button value
-    console.log(columns);
-    for(let j = 0; j  < columns; j++){
-      GenerateTable(columns);
+    let btnVal = e.target.value; //Num From size button
+    for(let j = 0; j  < btnVal; j++){
+      GenerateTable(btnVal); //calls the column
     }
-    const allPixels = document.querySelectorAll('.pixel-container');
-    Array.from(allPixels).map((i) => HoverEvent(i));
-    Array.from(allPixels).map((i) => SetSize(columns,i));
-    
-    SetSize()
+    const allPixels = document.querySelectorAll('.pixel');
+    Array.from(allPixels).map((i) => HoverEvent(btnVal,i));
+    Array.from(allPixels).map((i) => SetSize(btnVal,i));
+
   });
 }
 
-function HoverEvent(pixels){
-  pixels.addEventListener('mouseover', ()  => {
-    pixels.setAttribute('style','background-color: #364249;');
+function HoverEvent(btnVal,pixel){
+  pixel.addEventListener('mouseover', ()  => {
+    if(btnVal == 16){
+      pixel.setAttribute('style', ' background-color: #364249; height: 6.25%;');
+    }
+    else if(btnVal == 32){
+      pixel.setAttribute('style', 'background-color: #364249; height: 3.125%;')
+    }
+    else if(btnVal == 64){
+      pixel.setAttribute('style','background-color: #364249; height: 1.5625%;')
+    }
+
   });
 }
 
